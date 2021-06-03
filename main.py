2eecf17995215bcd8294c36f77816b53d95ad43c
Rahul Kumar <>
2021-06-03 16:34:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+life = 90-int(age)
+months = str(life*12)
+weeks = str(life*52)
+days = str(life*365)
+print("You have "+ days + " days, "+weeks+" weeks, "+months+" months"" to live")
+
+
+
+
+
+
+
